0e49bc6a163e5fd54afa0e0a8871dfba532cd07e
Yuvraj Singh <yuvrajsingh2537@gmail.com>
2025-07-28 11:42:14 +0530

    initial setup

---
+++ src/books/book_data.py
@@ -0,0 +1,12 @@
+books = [
+    {
+        "id": 1,
+        "title": "Think Python",
+        "author": "Allen B. Downey",
+        "publisher": "O'Reilly Media",
+        "published_date": "2021-01-01",
+        "page_count": 1234,
+        "language": "English",
+    },
+    # ... (other book entries)
+]
